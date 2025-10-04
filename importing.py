57bd274f61058df5e8c6f34e1a9ab9f263cb978f
Siddhi Raney <164665691+SiddhiRaney@users.noreply.github.com>
2025-10-04 20:42:24 +0530

    Create importing.py

---
+++ importing.py
@@ -0,0 +1,15 @@
+# Data analysis and Manipulation
+import plotly.graph_objs as go
+import plotly.io as pio
+import plotly.express as px
+import pandas as pd
+
+# Data Visualization
+import matplotlib.pyplot as plt
+
+# Importing Plotly
+import plotly.offline as py
+py.init_notebook_mode(connected=True)
+
+# Initializing Plotly
+pio.renderers.default = 'colab'

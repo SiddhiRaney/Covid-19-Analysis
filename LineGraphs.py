209cddff60111fcd52ce40edef34d986f9d12d8e
Siddhi Raney <164665691+SiddhiRaney@users.noreply.github.com>
2025-10-28 22:08:57 +0530

    Add line plots for COVID-19 case trends in the US

---
+++ LineGraphs.py
@@ -0,0 +1,17 @@
+import plotly.express as px
+
+# Line plot showing the trend of recovered cases over time in the US
+fig1 = px.line(df_US, x="Date", y="Recovered", height=400, title="Recovered Cases Over Time in the US")
+fig1.show()
+
+# Line plot showing the trend of deaths over time in the US
+fig2 = px.line(df_US, x="Date", y="Deaths", height=400, title="Deaths Over Time in the US")
+fig2.show()
+
+# Line plot showing the trend of confirmed cases over time in the US
+fig3 = px.line(df_US, x="Date", y="Confirmed", height=400, title="Confirmed Cases Over Time in the US")
+fig3.show()
+
+# Line plot showing the trend of new daily cases over time in the US
+fig4 = px.line(df_US, x="Date", y="New cases", height=400, title="New Cases Per Day in the US")
+fig4.show()
